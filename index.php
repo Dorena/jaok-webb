b8feceaa299353bf856de5abaeb402c9208f6510
dorena <johan.aslund.kdala@gmail.com>
2016-04-19 14:54:34 +0200

    allt

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Dank </title>
+    <link rel="stylesheet" href="CSS/css.css">
+    <meta charset="utf-8"/>
+  </head>
+
+
+
+<script>
+
+function test(){
+  hh = document.body.clientHeight - 94;
+  document.getElementById("wrapper2").style.height=hh+"px";
+}
+
+</script>
+
+    <body onLoad="test()">
+
+
+    <div id="wrapper">
+
+        <header>
+            <!-- Logga eller sidans namn-->
+            <h1 >  Weeb Cancer </h1>
+        </header>
+
+
+  <body>
+        <div id="wrapper2">
+        <nav>
+          <!-- navigatoin -->
+          <ul>
+            <li><a href="index.php"> Hem </a></li>
+            <li><a href="index.php?page=match"> Matcha </a></li>
+            <li><a href="index.php?page=profil"> Profil </a></li>
+            <li><a href="index.php?page=Skappa"> Skappa inlogg </a></li>
+          </ul>
+
+        </nav>
+
+
+
+
+
+          <main>
+            <div id="dank">
+            <img src="dank.png"/ width="200px" height="300px">
+          </div>
+
+
+
+        </main>
+
+      </div> <!-- wrapper -->
+
+
+      <div id="farbod">
+      </div>
+
+      <div id="andreas">
+    </div>
+
+    <div id="malte">
+    </div>
+  </body>
+
+</html>
